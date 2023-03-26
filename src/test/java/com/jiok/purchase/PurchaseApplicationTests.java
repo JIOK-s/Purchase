0f6be4818fbe4f1cdce74030b4f4403d0d23cfeb
hyunji-ok <hyunjiok10@gamil.com>
2023-03-26 15:14:31 +0900

    [feat] 개발환경 세팅 - spring initializer

---
+++ src/test/java/com/jiok/purchase/PurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiok.purchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
