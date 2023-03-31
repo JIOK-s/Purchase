9dddbeb4f97dd27070d7116f1cc3d9ab1606cba9
hyunji-ok <hyunjiok10@gamil.com>
2023-03-31 19:45:40 +0900

    [feat] querydsl 세팅 및 테스트

--- src/test/java/com/jiok/purchase/PurchaseApplicationTests.java
+++ src/test/java/com/jiok/purchase/PurchaseApplicationTests.java
@@ -1,13 +1,39 @@
 package com.jiok.purchase;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.jiok.purchase.entity.Hello;
+import com.jiok.purchase.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
 @SpringBootTest
+@Transactional
 class PurchaseApplicationTests {
 
+	@Autowired
+	EntityManager em;
+
 	@Test
+	@DisplayName("첫 테스트양")
 	void contextLoads() {
+
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+		QHello qHello = QHello.hello;
+
+		Hello result = queryFactory.selectFrom(qHello).fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
