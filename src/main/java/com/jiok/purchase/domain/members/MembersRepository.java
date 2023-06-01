d653e8c020b9d51e87067c63b289e6e6a7d24406
hyunji-ok <hyunjiok10@gamil.com>
2023-06-01 20:04:08 +0900

    [feat] jpaRepository와 custumRepository를 모두 사용할 수 있게 수정

--- src/main/java/com/jiok/purchase/domain/members/MembersRepository.java
+++ src/main/java/com/jiok/purchase/domain/members/MembersRepository.java
@@ -2,10 +2,8 @@ package com.jiok.purchase.domain.members;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MembersRepository {
+public interface MembersRepository extends JpaRepository<Members, Long>, MembersCustomRepository {
 
-    public Members findByMbrId(Long mbrId);
 
-    public Long save(Members reqMembers);
 
 }
